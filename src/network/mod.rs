ea0d71a6340361daa803c357bee829dec35f32ba
pca006132 <john.lck40@gmail.com>
2020-07-17 21:53:14 +0800

    Fixes compilation error, without considering the logic.

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,5 +1,6 @@
-use std::sync::mpsc::{Sender, Receiver, channel};
+use futures::channel::mpsc::{UnboundedSender, UnboundedReceiver, unbounded};
 use std::thread;
+use futures::sink::SinkExt;
 use tokio::net::TcpListener;
 use tokio::stream::StreamExt;
 use tokio_util::codec::{FramedWrite, LengthDelimitedCodec, FramedRead};
@@ -26,9 +27,9 @@ pub enum Packet {
 
 }
 
-pub fn create_background_loop() -> (Sender<Instruction>, Receiver<ResponseState>) {
-    let (to_background, from_foreground) = channel();
-    let (to_foreground, from_background) = channel();
+pub fn create_background_loop() -> (UnboundedSender<Instruction>, UnboundedReceiver<ResponseState>) {
+    let (to_background, from_foreground) = unbounded();
+    let (to_foreground, from_background) = unbounded();
     thread::spawn(move || {
         network_loop(from_foreground, to_foreground);
     });
@@ -36,7 +37,7 @@ pub fn create_background_loop() -> (Sender<Instruction>, Receiver<ResponseState>
 }
 
 #[tokio::main]
-async fn network_loop(from_foreground: Receiver<Instruction>, to_foreground: Sender<ResponseState>) {
+async fn network_loop(mut from_foreground: UnboundedReceiver<Instruction>, mut to_foreground: UnboundedSender<ResponseState>) {
     let mut listener = TcpListener::bind("0.0.0.0:4001").await.unwrap();
     let (tx, rx) = async_std::sync::channel(1);
     let server = async move {
@@ -78,7 +79,7 @@ async fn network_loop(from_foreground: Receiver<Instruction>, to_foreground: Sen
                     SymmetricalMessagePack::<Packet>::default(),
                 );
             loop {
-                while let Ok(inst) = from_foreground.try_recv() {
+                while let Ok(Some(inst)) = from_foreground.try_next() {
                     match inst {
                         Instruction::Disconnect(action) => {
                             match action {
@@ -92,11 +93,11 @@ async fn network_loop(from_foreground: Receiver<Instruction>, to_foreground: Sen
                     }
                 }
                 //mystery codes below
-                tokio::spawn(async move {
+                // tokio::spawn(async move {
                     while let Some(msg) = deserialized.try_next().await.unwrap() {
                         to_foreground.send(ResponseState::PacketReceived(msg));
                     }
-                }).await.unwrap();
+                // }).await.unwrap();
             }
         }
     });
