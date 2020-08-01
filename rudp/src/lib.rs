82da131fb7a064a9e8dcf03501906362121bf992
pca006132 <john.lck40@gmail.com>
2020-08-01 12:56:54 +0800

    Better error handling and more documentation.

--- rudp/src/lib.rs
+++ rudp/src/lib.rs
@@ -4,24 +4,25 @@ mod receiver;
 mod sender;
 
 use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-pub use protocol::{PacketDesc, DeserializeError};
+pub use protocol::{DeserializeError, PacketDesc};
 use receiver::Receiver;
 use sender::Sender;
 use std::marker::{Send, Sync};
 use std::thread;
-use tokio::{join, net::UdpSocket, time::Duration};
+use tokio::{net::UdpSocket, select, time::Duration};
 
 #[tokio::main]
 async fn udp_loop<T: PacketDesc + Send + Sync + 'static>(
     socket: UdpSocket,
     timeout: Duration,
     slot_capacity: usize,
+    max_retry: u32,
     drop_percentage: u64,
     from_fg: UnboundedReceiver<T>,
     to_fg: UnboundedSender<T>,
 ) {
     let (recv, send) = socket.split();
-    let mut sender = Sender::new(send, timeout, slot_capacity);
+    let mut sender = Sender::new(send, timeout, slot_capacity, max_retry);
     let mut receiver = Receiver::new(recv, &sender);
     let send_task = tokio::spawn(async move {
         let mut from_fg = from_fg;
@@ -31,15 +32,31 @@ async fn udp_loop<T: PacketDesc + Send + Sync + 'static>(
         let mut to_fg = to_fg;
         receiver.recv_loop(&mut to_fg, drop_percentage).await;
     });
-    let _ = join!(send_task, recv_task);
+    // Close the task when any finishes.
+    select!(
+        _ = send_task => (),
+        _ = recv_task => ()
+    );
 }
 
+/// Start the UDP loop.
+/// # Parameters
+/// * socket: Socket for communication, should be connected already.
+/// * timeout: Timeout for retransmission.
+/// * slot_capacity: Number of slots for sending reliable packets *in parallel*.
+/// * max_retry: Maximum number of consecutive send attempts when the socket is unable to send messages.
+///   If reached, the send task would exit. Note that this is not resend attempt.
+/// * drop_percentage: Packet drop rate for simulating packet drop. If 0, it would not attemp to
+///   simulate packet drop. Should be within 0..100. Note that the probability is not really that
+///   accurate, this is for testing only.
 pub fn start_udp_loop<T: PacketDesc + Send + Sync + 'static>(
     socket: UdpSocket,
     timeout: Duration,
     slot_capacity: usize,
+    max_retry: u32,
     drop_percentage: u64,
 ) -> (UnboundedSender<T>, UnboundedReceiver<T>) {
+    debug_assert!(drop_percentage < 100);
     let (to_background, from_foreground) = unbounded();
     let (to_foreground, from_background) = unbounded();
     thread::spawn(move || {
@@ -47,6 +64,7 @@ pub fn start_udp_loop<T: PacketDesc + Send + Sync + 'static>(
             socket,
             timeout,
             slot_capacity,
+            max_retry,
             drop_percentage,
             from_foreground,
             to_foreground,
