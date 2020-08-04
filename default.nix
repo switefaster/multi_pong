69350d41a02f7419d86856e8574007a35f78accd
pca006132 <john.lck40@gmail.com>
2020-08-04 20:14:47 +0800

    Added nix build script.

---
+++ default.nix
@@ -0,0 +1,50 @@
+{ mozillaOverlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+, pkgs ? import <nixpkgs> { overlays = [ mozillaOverlay ]; } }:
+let
+  rust = pkgs.latest.rustChannels.nightly.rust;
+  rustPlatform = (pkgs.makeRustPlatform {
+    rustc = rust;
+    cargo = rust;
+  });
+in
+rustPlatform.buildRustPackage rec {
+  pname = "multi_pong";
+  version = "0.1.0";
+  src = ./.;
+  cargoSha256 = "1wnzf6cx9mz97big7d724g2h8xlg5xaxflddsxzicfca4yr9qqni";
+  nativeBuildInputs = (
+    with pkgs; [
+      alsaLib
+      cmake
+      freetype
+      expat
+      openssl
+      pkgconfig
+      python3
+      (
+        vulkan-validation-layers.overrideAttrs (
+          old: {
+            setupHook = writeText "setup-hook" ''
+              addToSearchPath XDG_DATA_DIRS @out@/share
+              export XDG_DATA_DIRS
+            '';
+          }
+        )
+      )
+      xlibs.libX11
+    ]
+  );
+  APPEND_LIBRARY_PATH = (
+    with pkgs; stdenv.lib.makeLibraryPath [
+      vulkan-loader
+      xlibs.libXcursor
+      xlibs.libXi
+      xlibs.libXrandr
+    ]
+  );
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
+    export RUSTFLAGS="-C target-cpu=native"
+  '';
+}
